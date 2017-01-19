fe3552f1d31716a73dd174bb01b5663b4c43ac0b
Daniel Chołuj <gdanielch9@gmail.com>
2017-01-19 17:52:40 +0100

    lab6

---
+++ Lab6/Lectures/DataAccess/VSProject/UniversityDataAccessProject/DomainModel/Entities/Controller.cs
@@ -0,0 +1,32 @@
+﻿namespace DomainModel.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+
+    [Table("Controller")]
+    public class Controller
+    {
+        [Key]
+        public int ControllerKey { get; set; }
+
+        [MaxLength(20)]
+        public string Serial { get; set; }
+
+        // foreign key convention.
+        [Index("IX_GatewayId")]
+        public int GatewayId { get; set; }
+
+        public Gateway Gateway { get; set; }
+
+        public ControllerStatus Status { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
+        public int ControllerTypeKey { get; set; }
+
+        [ForeignKey(nameof(ControllerTypeKey))]
+        public ControllerType ControllerType { get; set; }
+    }
+}
---
+++ Lab6/Lectures/DataAccess/VSProject/UniversityDataAccessProject/DomainModel/Entities/ControllerType.cs
@@ -0,0 +1,31 @@
+﻿namespace DomainModel.Entities
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Controller Type
+    /// </summary>
+    [Table("ControllerType")]
+    public class ControllerType
+    {
+        public ControllerType()
+        {
+            this.Datapoints = new List<Datapoint>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Category { get; set; }
+
+        public int HardwareIndex { get; set; }
+
+        public int SoftwareIndexMin { get; set; }
+
+        public int SoftwareIndexMax { get; set; }
+
+        public IList<Datapoint> Datapoints { get; set; }
+    }
+}
